d3153e847f355850b4b7276b4f2538d3ef501172
ornitorrincco <ornitorrincco@gmail.com>
2018-01-12 09:53:41 -0800

    init project added

---
+++ GREapp/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text, WebView } from 'react-native';
+
+class App extends Component {
+
+  render() {
+    return (
+      <View>
+      <Text>An a app </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
